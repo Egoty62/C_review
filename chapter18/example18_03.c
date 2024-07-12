d360bf39d9bd4b4b1857f830ac189b5a58450051
Egoty62 <myosotis62@naver.com>
2024-07-12 11:13:55 +0900

    Chapter_18

--- chapter18/example18_03.c
+++ chapter18/example18_03.c
@@ -8,5 +8,73 @@
 int main()
 {
     FILE *ifp, *ofp;
-    // a+, fseek(rewind) 사용하기
+    char bookname[80];
+    char *str[11] = {0};
+    int sum[11] = {0, };
+    double cost;
+    int i, j, cnt;
+    int temp;
+
+    ifp = fopen("C:\\C (git)\\Chapter18\\ex1803-01.txt", "r");
+    if(ifp == NULL) return 1;
+
+    fgets(bookname, sizeof(bookname), ifp);
+    bookname[strlen(bookname) - 1] = '\0';
+    fscanf(ifp, "%lf %d", cost, cnt);
+    fgetc(ifp);         // 개행문자 제거
+    // 반복문 시작
+    for(i = 0; i < 11; i++)
+    {
+        if(str[i] == NULL)
+        {
+            str[i] = (char *)malloc(strlen(bookname));
+            strcpy(str[i], bookname);
+            sum[i] = (int)(cost * cnt * 10000);
+            break;
+        }
+
+        if(strcmp(str[i], bookname) == 0)
+        {
+            sum[i] += (int)(cost * cnt * 10000);
+            break;
+        }
+    }
+
+    for(i = 0; i < 11; i++)
+    {
+        if(str[i] == NULL) break;
+        for(j = i + 1; j < 11; j++)
+        {
+            if (str[j] == NULL) break;
+            if(sum[i] < sum[j])
+            {
+                temp = sum[j];
+                sum[j] = sum[i];
+                sum[i] = temp;
+
+                strcpy(bookname, str[j]);
+                strcpy(str[j], str[i]);
+                strcpy(str[i], bookname);
+            }
+        }
+    }
+    
+    ofp = fopen("C:\\C (git)\\Chapter18\\ex1803-02.txt", "w");
+    if(ofp == NULL) return 1;
+
+    for(i = 0; i < 11; i++)
+    {
+        if(str[i] == NULL) break;
+        fprintf(ofp, "%d. %s - %d\n", i + 1, str[i], sum[i]);
+    }
+    
+    for(i = 0; i < 11; i++)
+    {
+        free(str[i]);
+    }
+    // 반복문 종료
+    fclose(ifp);
+    fclose(ofp);
+
+    return 0;
 }
